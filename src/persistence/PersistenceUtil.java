a7dcb36eaca4fdff5dff5bdeaf4af97f86e471fc
josue <josue@10.204.23.226>
2017-09-01 17:17:14 -0300

    teste

---
+++ src/persistence/PersistenceUtil.java
@@ -0,0 +1,18 @@
+package persistence;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class PersistenceUtil {
+	
+	private static EntityManagerFactory factory;
+	
+	public static EntityManager getEntityManager()
+	{
+		if (factory == null)
+			factory = Persistence.createEntityManagerFactory("translog");
+		
+		return factory.createEntityManager();
+	}
+}
